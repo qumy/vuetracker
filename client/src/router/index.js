724bb0ada342d07c719903735f7233e473f3724f
qumy <qumy@yandex.ru>
2018-08-18 10:54:37 +0500

    Added songs components

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,25 +3,43 @@ import Router from 'vue-router'
 import Login from '@/components/Login'
 import Register from '@/components/Register'
 import Songs from '@/components/Songs'
+import ViewSong from '@/components/ViewSong'
+import CreateSong from '@/components/CreateSong'
+import EditSong from '@/components/EditSong'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
       path: '/register',
       name: 'register',
       component: Register
     },
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
       path: '/songs',
       name: 'songs',
       component: Songs
+    },
+    {
+      path: '/songs/create',
+      name: 'songs-create',
+      component: CreateSong
+    },
+    {
+      path: '/songs/:songId',
+      name: 'song',
+      component: ViewSong
+    },
+    {
+      path: '/songs/:songId/edit',
+      name: 'songs-edit',
+      component: EditSong
     },
     {
       path: '*',
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,10 +1,12 @@
 const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
 
 module.exports = (app) => {
-  app.post('/register',
-    AuthenticationController.register,
-    AuthenticationControllerPolicy.register)
-  app.post('/login',
-    AuthenticationController.login)
+  app.post('/register', AuthenticationController.register, AuthenticationControllerPolicy.register)
+  app.post('/login', AuthenticationController.login)
+  app.get('/songs', SongsController.index)
+  app.get('/songs/:songId', SongsController.show)
+  app.put('/songs/:songId', SongsController.put)
+  app.post('/songs', SongsController.post)
 }
